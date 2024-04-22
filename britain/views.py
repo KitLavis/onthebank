43161b41abc5d7ed1ce60b91c45f421758f3323b
Kit Lavis <kcjlavis@gmail.com>
2024-04-22 15:58:58 +0200

    Geojson feature now showing on map

--- britain/views.py
+++ britain/views.py
@@ -3,7 +3,7 @@ from .models import WatercourseLink
 
 
 def index(request):
-    river_avon = WatercourseLink.objects.filter(name1='River Avon')
+    test_link = WatercourseLink.objects.get(identifier='DC6C6B09-67D0-4F01-9089-F4E8C5D0DEBF')
     context = {}
-    context['river_avon'] = river_avon
+    context['test_link'] = test_link
     return render(request, 'index.html', context)
