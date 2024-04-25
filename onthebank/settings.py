96fda138422814bd19daa7212d783789c45a3309
Kit Lavis <kcjlavis@gmail.com>
2024-04-25 16:09:06 +0200

    Django secret key added to env file

--- onthebank/settings.py
+++ onthebank/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e-)wn8g!np@2ge&xfl^y(7)5t48dlxmh2$6&@1iy=ui=mtn=y4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
