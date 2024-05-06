a63a66ee363053b80024f3aa40b4b785a15f010a
Kit Lavis <kcjlavis@gmail.com>
2024-05-06 15:45:48 +0200

    Heroku added to allowed hosts

--- onthebank/settings.py
+++ onthebank/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['on-the-bank-a0d7dcc1d632.herokuapp.com']
 
 
 # Application definition
