466e2066d94b9322ff997491af01e466e7f6a088
Kit Lavis <kcjlavis@gmail.com>
2024-05-06 15:50:39 +0200

    Debug true to find error

--- onthebank/settings.py
+++ onthebank/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['on-the-bank-a0d7dcc1d632.herokuapp.com']
 
